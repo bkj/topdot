6d659b500d4c3d3872949a671ede9eb985f8d5d5
Ben Johnson <bkj.322@gmail.com>
2018-09-21 11:16:19 -0400

    cleaning up test

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# run.sh
+
+python test.py --dim 4096 --k 512
+
+python test.py --dim 8192 --k 1024
+
+python test.py --dim 16284 --k 1024
--- test.py
+++ test.py
@@ -1,10 +1,21 @@
-from time import time
-import numpy as np
-from scipy.sparse import csr_matrix
-from scipy.sparse import rand
-import topdot.topdot as ct
+#!/usr/bin/env python
 
-def run(A, B, k, lower_bound=0):
+"""
+    test.py
+"""
+
+from __future__ import print_function, division
+
+import sys
+import json
+import argparse
+import numpy as np
+from time import time
+from scipy import sparse
+
+import topdot.topdot as td
+
+def run_topdot(A, B, k, lower_bound=0):
     A = A.tocsr()
     B = B.tocsr()
     
@@ -12,7 +23,7 @@ def run(A, B, k, lower_bound=0):
     
     I = np.empty(num_rows * k, dtype=np.int32)
     D = np.empty(num_rows * k, dtype=A.dtype)
-    ct.topdot(
+    td.topdot(
         n_row=num_rows,
         n_col=B.shape[1],
         
@@ -36,44 +47,95 @@ def run(A, B, k, lower_bound=0):
     
     return D, I
 
-def _slow_version(csr_row, ntop):
+def _run_naive(csr_row, ntop):
     nnz = csr_row.getnnz()
     if nnz == 0:
-        return None
+        return (None, None)
     elif nnz <= ntop:
-        result = zip(gt_csr_row.indices, csr_row.data)
+        return csr_row.data, csr_row.indices
     else:
         arg_idx = np.argpartition(csr_row.data, -ntop)[-ntop:]
-        result = zip(csr_row.indices[arg_idx], csr_row.data[arg_idx])
-    
-    return list(result)
+        return csr_row.indices[arg_idx], csr_row.data[arg_idx]
 
-def slow_version(A, B, ntop, lower_bound=0):
+def run_naive(A, B, ntop, lower_bound=None):
     C = A.dot(B)
-    return [_slow_version(row, ntop) for row in C]
+    I, D = zip(*[_run_naive(row, ntop) for row in C])
+    return D, I
 
-np.random.seed(123)
-
-N = 14
-a = rand(2 ** N, 2 ** N, density=0.01, format='csr')
-b = rand(2 ** N, 2 ** N, density=0.01, format='csr')
-
-t = time()
-D, I = run(a, b, 1000)
-print(time() - t)
+# --
+# Run
 
 
-t = time()
-cc = slow_version(a, b, 1000)
-print(time() - t)
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dim', type=int, default=4096)
+    parser.add_argument('--k', type=int, default=128)
+    parser.add_argument('--density', type=float, default=0.01)
+    parser.add_argument('--seed', type=int, default=123)
+    return parser.parse_args()
 
+if __name__ == "__main__":
+    args = parse_args()
+    np.random.seed(args.seed)
 
-for i in range(2 ** N):
-    assert sorted([ccc[0] for ccc in cc[i]]) == sorted(I[i])
-
-
-t = time()
-cc = a.dot(b)
-print(time() - t)
-
-
+    t = time()
+    A = sparse.rand(args.dim, args.dim, density=args.density, format='csr')
+    B = sparse.rand(args.dim, args.dim, density=args.density, format='csr')
+    A *= 10
+    B *= 10
+    gen_time = time() - t
+    print('gen_time  ', gen_time, file=sys.stderr)
+    
+    t = time()
+    td_D, td_I = run_topdot(A, B, args.k)
+    td_time = time() - t
+    print('td_time   ', td_time, file=sys.stderr)
+    
+    t = time()
+    na_D, na_I = run_naive(A, B, args.k)
+    naive_time = time() - t
+    print('naive_time', naive_time, file=sys.stderr)
+    
+    rand_idx = np.random.choice(args.dim, args.k, replace=False)
+    for idx in rand_idx:
+        na_idx = sorted(na_I[idx])
+        td_idx = sorted(td_I[idx])
+        assert (na_idx == td_idx), "(na_idx != td_idx)"
+    
+    t = time()
+    cc = A.dot(B)
+    dot_time = time() - t
+    print('dot_time  ', dot_time, file=sys.stderr)
+    
+    print(json.dumps({
+        "gen_time"   : gen_time,
+        "td_time"    : td_time,
+        "naive_time" : naive_time,
+        "dot_time"   : dot_time,
+    }))
+    
+    # # >>
+    # # FAISS w/ dense matrices
+    # import faiss
+    # findex  = faiss.IndexFlatIP(args.dim)
+    # Ad = np.asarray(A.todense()).astype(np.float32)
+    # Bd = np.asarray(B.todense()).astype(np.float32)
+    # Bd = np.ascontiguousarray(Bd.T)
+    # findex.add(Bd)
+    
+    # t = time()
+    # faiss_D, faiss_I = findex.search(Ad, args.k)
+    # faiss_brute_time = time() - t
+    # print('faiss_brute_time', faiss_brute_time)
+    
+    # rand_idx = np.random.choice(args.dim, args.k, replace=False)
+    # for idx in rand_idx:
+    #     faiss_idx = sorted(na_I[idx])
+    #     td_idx    = sorted(td_I[idx])
+    #     assert (faiss_idx == td_idx), "(faiss_idx != td_idx)"
+    # # <<
+    
+    # # >>
+    # NMSLIB w/ sparse matrices
+    # ... slower than brute force? ...
+    # # <<
